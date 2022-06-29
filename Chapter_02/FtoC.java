8407f3d521723c7045e05c223480a159ba337e13
Jared Moise <55561624+svveden@users.noreply.github.com>
2022-06-29 09:52:14 -0500

    Create FtoC.java

---
+++ Chapter_02/FtoC.java
@@ -0,0 +1,10 @@
+//FtoC
+
+public class FtoC {
+	public static void main(String[] args){
+		double fahrenheit = 98.6;
+		double celsius = (fahrenheit - 32.0) * (5.0 / 9.0);
+		System.out.print("Celcius equivalent: ");
+		System.out.println(celsius);
+	}
+}
